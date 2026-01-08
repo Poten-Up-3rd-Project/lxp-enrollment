c4612297f3ac0593e8389c7ae424a5875ef48b26
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-08 17:52:19 +0900

    feat(enrollment): JpaEntity (+ Jpa Embeddable VO) 구현

---
+++ src/main/java/com/lxp/enrollment/infra/adapter/out/persistence/jpa/model/CancelDetailsJpaEmbeddable.java
@@ -0,0 +1,47 @@
+package com.lxp.enrollment.infra.adapter.out.persistence.jpa.model;
+
+import com.lxp.enrollment.domain.model.enums.CancelReasonType;
+import com.lxp.enrollment.domain.model.enums.CancelType;
+import com.lxp.enrollment.domain.model.vo.CancelDetails;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CancelDetailsJpaEmbeddable {
+
+    private Instant canceledAt;
+    @Enumerated(EnumType.STRING)
+    private CancelType cancelType;
+    @Enumerated(EnumType.STRING)
+    private CancelReasonType cancelReasonType;
+    private String cancelReasonComment;
+
+    private CancelDetailsJpaEmbeddable(
+            Instant canceledAt,
+            CancelType cancelType,
+            CancelReasonType cancelReasonType,
+            String cancelReasonComment
+    ) {
+        this.canceledAt = canceledAt;
+        this.cancelType = cancelType;
+        this.cancelReasonType = cancelReasonType;
+        this.cancelReasonComment = cancelReasonComment;
+    }
+
+    public static CancelDetailsJpaEmbeddable of(CancelDetails cancelDetails) {
+        return new CancelDetailsJpaEmbeddable(
+                cancelDetails.cancelledAt(),
+                cancelDetails.cancelType(),
+                cancelDetails.cancelReasonType(),
+                cancelDetails.cancelReasonComment()
+        );
+    }
+}
