c882eb6616fcf2839eda73efc844fb391d6e3222
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-11 20:56:59 +0900

    fix(enrollment): 전 범위로 수정

--- src/main/java/com/lxp/Application.java
+++ src/main/java/com/lxp/Application.java
@@ -1,12 +1,11 @@
 package com.lxp;
 
-import com.lxp.enrollment.infra.adapter.provided.web.external.passport.PassportKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan(basePackageClasses = {PassportKeyProperties.class})
+@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {
