3153906077ef887750c3bd527e9b2baaa3992e18
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-12 12:40:19 +0900

    fix(enrollment): @Controller -> @RestController

--- src/main/java/com/lxp/enrollment/infra/adapter/provided/web/external/EnrollmentController.java
+++ src/main/java/com/lxp/enrollment/infra/adapter/provided/web/external/EnrollmentController.java
@@ -16,7 +16,6 @@ import com.lxp.enrollment.infra.adapter.provided.web.external.response.EnrollCou
 import com.lxp.enrollment.infra.adapter.provided.web.external.response.EnrollmentDetailsResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,10 +23,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/api-v1/enrollments")
 public class EnrollmentController {
 
