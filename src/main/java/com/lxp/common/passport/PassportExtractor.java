efb9c94ac1081690f44f221e35dac3fe5fc485d4
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-14 14:14:14 +0900

    application-local.yml 더 이상 올리지 않을 예정
    로컬에서 값 바꿀 때마다 자꾸 변경사항 뜨는 것 막기 위함

---
+++ src/main/java/com/lxp/common/passport/PassportExtractor.java
@@ -0,0 +1,13 @@
+package com.lxp.common.passport;
+
+import com.lxp.common.passport.constant.PassportConstants;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PassportExtractor {
+
+    public String extract(HttpServletRequest request) {
+        return request.getHeader(PassportConstants.PASSPORT_HEADER_NAME);
+    }
+}
