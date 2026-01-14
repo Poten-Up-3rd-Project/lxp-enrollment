c2ca6928a736b22b826d8c6b76334c0d312f03c5
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-14 10:33:53 +0900

    feat(enrollment): passport 관련 상수 인터페이스 정의

---
+++ src/main/java/com/lxp/common/passport/constant/PassportConstants.java
@@ -0,0 +1,12 @@
+package com.lxp.common.passport.constant;
+
+public interface PassportConstants {
+
+    // Passport 헤더 이름
+    String PASSPORT_HEADER_NAME = "X-Passport";
+
+    // JWT Claims
+    String PASSPORT_USER_ID = "uid";
+    String PASSPORT_ROLE = "rol";
+    String PASSPORT_TRACE_ID = "tid";
+}
