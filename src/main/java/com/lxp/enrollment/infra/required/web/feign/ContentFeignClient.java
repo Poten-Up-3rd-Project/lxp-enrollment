a331b52aec2fea70713857f632514d07ffbd1f18
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-15 15:02:43 +0900

    feat(enrollment): feign 클라이언트를 통한 api 호출 시 에러(4xx, 5xx)가 가 응답된 경우의 핸들러 정의

--- src/main/java/com/lxp/enrollment/infra/required/web/feign/ContentFeignClient.java
+++ src/main/java/com/lxp/enrollment/infra/required/web/feign/ContentFeignClient.java
@@ -1,11 +1,16 @@
 package com.lxp.enrollment.infra.required.web.feign;
 
+import com.lxp.enrollment.infra.required.web.feign.config.FeignErrorConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "content-service", url = "${services.content-service.url}")
+@FeignClient(
+        name = "content-service",
+        url = "${services.content-service.url}/internal/api-v1",
+        configuration = FeignErrorConfig.class
+)
 public interface ContentFeignClient {
 
     @GetMapping("/courses/{courseId}/exists")
