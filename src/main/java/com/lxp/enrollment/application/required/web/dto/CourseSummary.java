ede06524a46120f6dc8813b897409ad399e2e539
SeungYeop-Han <ammezkhan@gmail.com>
2026-01-19 13:44:04 +0900

    fix(enrollment): 내부 api 스키마 일치하지 않는 부분 해결

--- src/main/java/com/lxp/enrollment/application/required/web/dto/CourseSummary.java
+++ src/main/java/com/lxp/enrollment/application/required/web/dto/CourseSummary.java
@@ -1,25 +1,36 @@
 package com.lxp.enrollment.application.required.web.dto;
 
+import java.time.Instant;
 import java.util.List;
 
 public record CourseSummary(
         String courseId,
         String thumbnailUrl,
         Double totalProgress,
-        String courseTitle,
-        String courseDescription,
-        String instructorName,
+        String title,
+        String description,
+        CourseInstructor Instructor,
         String level,
+        Instant createdAt,
+        Instant updatedAt,
         List<CourseTag> tags
 ) {
+    // content == 태그 이름
+    // To Do: 태그 - category, subCategory, State 필요하다고 민영님한테 말씀드릴 것
+    // To Do: 강사 id 도 응답에 포함 시키자
     public record CourseTag(
             String category,
             String subCategory,
-            Long tagId,
-            String name,
-            String state,
+            Long id,
+            String content,
             String color,
             String variant
+    ) {
+    }
+
+    public record CourseInstructor(
+            String instructorId,
+            String name
     ) {
     }
 }
